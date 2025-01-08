015e94b38bea67935ec9de2a2245cc69a941d083
cindy <cindy.septia0919@gmail.com>
2025-01-08 10:44:36 +0700

    feat: add style

--- main.py
+++ main.py
@@ -27,9 +27,12 @@ def main():
     view.setWindowIcon(QIcon('assets/icon.svg'))
     # Tampilkan window utama
     view.show()
+    with open("view/style.qss", "r") as file:
+        app.setStyleSheet(file.read())
 
     # Mulai aplikasi
     sys.exit(app.exec())
+
 
 if __name__ == "__main__":
     main()
