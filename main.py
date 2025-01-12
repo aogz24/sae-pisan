c245044a72df3b8e898edace7b75546fbff538bc
Agus Riyanto <ragus8188@gmail.com>
2025-01-12 21:32:45 +0700

    feat(main): add R installation logic if not present

--- main.py
+++ main.py
@@ -9,9 +9,22 @@ from view.components.CsvDialogOption import CSVOptionsDialog
 # from model.SummaryData import SummaryData
 import polars as pl
 import os
+import subprocess
 
 def main():
-    path= os.path.join(os.path.dirname(__file__), 'R', 'R-4.4.2')
+    path = os.path.join(os.path.dirname(__file__), 'R', 'R-4.4.2')
+    if not os.path.exists(path):
+        url = "https://cran.r-project.org/bin/windows/base/R-4.4.2-win.exe"
+        installer_path = os.path.join(os.path.expanduser("~"), "R-latest.exe")
+
+        subprocess.check_call(['curl', '-o', installer_path, url])
+
+        # Define the relative path for the R installation directory
+        relative_path = os.path.join(os.path.dirname(__file__), 'R', 'R-4.4.2')
+
+        # Run the installer silently with the correct directory
+        # Run the installer silently with the correct directory
+        subprocess.check_call([installer_path, '/SILENT', f'/DIR={relative_path}'])
     os.environ['R_HOME'] = path
     from service.main.LoadingR import loadR
     app = QApplication(sys.argv)
