2c195795a4821f2f01cb2cd15c085073b585b1e9
cindy <cindy.septia0919@gmail.com>
2025-01-13 15:47:16 +0700

    feat: Improve check_environment function to support R installation on both Windows and POSIX systems

--- service/main/CheckEnviroment.py
+++ service/main/CheckEnviroment.py
@@ -4,6 +4,7 @@ import time
 import errno
 
 def check_environment(path):
+    print(path)
     if not os.path.exists(path):
         if os.name == 'nt':
             url = "https://cran.r-project.org/bin/windows/base/R-4.4.2-win.exe"
@@ -37,18 +38,4 @@ def check_environment(path):
             subprocess.check_call(['./configure', '--prefix=' + path])
             subprocess.check_call(['make'])
             subprocess.check_call(['make', 'install'])
-    # os.environ['R_HOME'] = path
-
-    # Make R_HOME permanent by adding it to the user's environment variables
-    if os.name == 'nt':
-        subprocess.check_call(['setx', 'R_HOME', path])
-    elif os.name == 'posix':
-        bashrc_path = os.path.join(os.path.expanduser("~"), '.bashrc')
-        with open(bashrc_path, 'r') as bashrc:
-            lines = bashrc.readlines()
-        with open(bashrc_path, 'w') as bashrc:
-            for line in lines:
-                if line.strip().startswith('export R_HOME='):
-                    continue
-                bashrc.write(line)
-            bashrc.write(f'\nexport R_HOME={path}\n')
+    os.environ['R_HOME'] = path
--- service/main/LoadingR.py
+++ service/main/LoadingR.py
@@ -8,7 +8,7 @@ def loadR(splash):
             suppressPackageStartupMessages({
                 r_home <- Sys.getenv("R_HOME")
                 if (!require("sae", quietly = TRUE)) install.packages("sae", lib=r_home); library(sae, lib.loc=r_home);
-                if (!require("polars", quietly = TRUE)) install.packages("polars", repos = "https://community.r-multiverse.org", lib=r_home);
+                if (!require("polars", quietly = TRUE)) install.packages("sae", lib=r_home); library(sae, lib.loc=r_home);
                 if (!require("arrow", quietly = TRUE)) install.packages("arrow", lib=r_home);
                 if (!require("sae.projection", quietly = TRUE)) install.packages("sae.projection", lib=r_home); library(sae.projection, lib.loc=r_home);
             })
