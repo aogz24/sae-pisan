d03c70d7ec5f8d75c48f294690454a178e15e19a
Agus Riyanto <ragus8188@gmail.com>
2025-01-22 09:53:12 +0700

    feat: Add run_model and get_model2 methods to SaeHB class

--- model/SaeHB.py
+++ model/SaeHB.py
@@ -3,3 +3,10 @@ from SaeModelling import SaeModelling
 class SaeHB(SaeModelling):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        
+    def run_model(self, r_script):
+        self.r_script = r_script
+        # run_model_eblup_area(self)
+    
+    def get_model2(self):
+        return self.model2
