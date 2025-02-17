c0b981d000857a53af16857b86b574e26e5fbb2f
cindy <cindy.septia0919@gmail.com>
2025-02-17 14:05:24 +0700

    refactor: Update MulticollinearityDialog script layout and add running icon

--- view/components/MulticollinearityDialog.py
+++ view/components/MulticollinearityDialog.py
@@ -1,7 +1,9 @@
 from PyQt6.QtWidgets import (
-    QDialog, QVBoxLayout, QHBoxLayout, QListView, QPushButton, QLabel, QComboBox, QCheckBox, QTextEdit, QGroupBox, QMessageBox
+    QDialog, QVBoxLayout, QHBoxLayout, QListView, QPushButton, QLabel, QCheckBox, QTextEdit, QGroupBox, QMessageBox, QMessageBox, QSpacerItem, QSizePolicy
 )
-from PyQt6.QtCore import Qt, QStringListModel
+from PyQt6.QtGui import QIcon
+import polars as pl
+from PyQt6.QtCore import Qt, QStringListModel, QSize
 from model.Multicollinearity import Multicollinearity
 from controller.Eksploration.EksplorationController import MulticollinearityController
 
@@ -130,9 +132,23 @@ class MulticollinearityDialog(QDialog):
         main_layout.addLayout(content_layout)
 
         # Script box
-        self.script_label = QLabel("Script:", self)
+        self.script_layout = QHBoxLayout() 
+        self.script_label = QLabel("R Script:", self)
+
+        self.icon_label = QLabel(self)
+        self.icon_label.setPixmap(QIcon("assets/running.svg").pixmap(QSize(16, 30)))
+        self.icon_label.setFixedSize(16, 30)
+        self.icon_label.setAlignment(Qt.AlignmentFlag.AlignRight)
+
+        spacer = QSpacerItem(40, 10, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
+
+        self.script_layout.addWidget(self.script_label)
+        self.script_layout.addItem(spacer)  
+        self.script_layout.addWidget(self.icon_label)
+        self.icon_label.setVisible(False)
         self.script_box = QTextEdit(self)
-        main_layout.addWidget(self.script_label)
+
+        main_layout.addLayout(self.script_layout)  
         main_layout.addWidget(self.script_box)
 
         # Tombol Run
@@ -157,11 +173,11 @@ class MulticollinearityDialog(QDialog):
 
 
     def get_column_with_dtype(self, model):
-        """Mengembalikan daftar nama kolom dengan tipe datanya"""
-        return [
-            f"{col} [numerik]" if dtype in ['int64', 'float64'] else f"{col} [{dtype}]"
+        self.columns = [
+            f"{col} [{dtype}]" if dtype == pl.Utf8 else f"{col} [Numeric]"
             for col, dtype in zip(model.get_data().columns, model.get_data().dtypes)
         ]
+        return self.columns 
     
 
     def add_variable_dependent_variable(self):
@@ -174,35 +190,51 @@ class MulticollinearityDialog(QDialog):
         selected_indexes = self.data_editor_list.selectedIndexes() + self.data_output_list.selectedIndexes()
         selected_items = [index.data() for index in selected_indexes]
 
+        # Jika tidak ada yang dipilih
         if not selected_items:
             QMessageBox.warning(self, "Warning", "Please select a variable first!")
-            return  # Show a warning and exit if no variable is selected
+            return
 
-        selected_list = self.dependent_variable_model.stringList()
+        if len(selected_items) > 1:
+            QMessageBox.warning(self, "Warning", "Please select only one variable!")
+            return
 
-        for item in selected_items:
-            if item in self.data_editor_model.stringList():
-                editor_list = self.data_editor_model.stringList()
-                editor_list.remove(item)
-                self.data_editor_model.setStringList(editor_list)
-            elif item in self.data_output_model.stringList():
-                output_list = self.data_output_model.stringList()
-                output_list.remove(item)
-                self.data_output_model.setStringList(output_list)
+        item = selected_items[0]
 
-            if item not in selected_list:
-                selected_list = [item]  # Ensure only one variable is in the dependent_variable axis
-                break  # Exit loop after adding one item
+        if "[String]" in item:
+            QMessageBox.warning(self, "Warning", "Selected variable must be of type Numeric.")
+            return
 
-        self.dependent_variable_model.setStringList(selected_list)
+        # Pindahkan item dari data_editor atau data_output
+        if item in self.data_editor_model.stringList():
+            editor_list = self.data_editor_model.stringList()
+            editor_list.remove(item)
+            self.data_editor_model.setStringList(editor_list)
+        elif item in self.data_output_model.stringList():
+            output_list = self.data_output_model.stringList()
+            output_list.remove(item)
+            self.data_output_model.setStringList(output_list)
+
+        # Masukkan ke dependent_variable_model
+        self.dependent_variable_model.setStringList([item])
+
+        # Generate R script setelah variabel ditambahkan
         self.generate_r_script()
+
 
     
     def add_variable_independent_variables(self):
-        # Ambil semua indeks yang dipilih dari data editor dan data output
+
         selected_indexes = self.data_editor_list.selectedIndexes() + self.data_output_list.selectedIndexes()
         selected_items = [index.data() for index in selected_indexes]
         selected_list = self.independent_variable_model.stringList()
+
+        contains_string = any("[String]" in item for item in selected_items)
+        selected_items = [item for item in selected_items if "[String]" not in item]
+
+        
+        if contains_string:
+            QMessageBox.warning(None, "Warning", "Selected variables must be of type Numeric.")
 
         for item in selected_items:
             if item in self.data_editor_model.stringList():
@@ -283,8 +315,16 @@ class MulticollinearityDialog(QDialog):
     def accept(self):
         r_script = self.script_box.toPlainText()
         if not r_script:
+            QMessageBox.warning(self, "Empty Script", "Please generate a script before running.")
+            return
+
+        if len(self.get_selected_independent_variables()) < 2:
+            QMessageBox.warning(self, "Invalid Independent Variables", "Please select at least two independent variables.")
             return
         
+        self.run_button.setText("Running...")
+        self.icon_label.setVisible(True)
+
         multicollinearity = Multicollinearity(self.model1, self.model2, self.parent)
         
         if self.regression_line_checkbox.isChecked():
@@ -294,8 +334,9 @@ class MulticollinearityDialog(QDialog):
         controller.run_model(r_script)
 
         self.parent.add_output(script_text=r_script, result_text=multicollinearity.result)
-        self.run_button.setEnabled(True)
+        self.icon_label.setVisible(False)
         self.run_button.setText("Run")
+        QMessageBox.information(self, "Multicollinearity", "Multicollinearity analysis has been completed.")
         self.close()
 
     def closeEvent(self, event):
@@ -311,11 +352,9 @@ class MulticollinearityDialog(QDialog):
         dependent_var = self.get_selected_dependent_variable()
         independent_vars = self.get_selected_independent_variables()
 
-        if not dependent_var:
-            self.script_box.setPlainText("No dependent variable selected.")
-            return
-        if not independent_vars:
-            self.script_box.setPlainText("No independent variables selected.")
+        # Check if dependent_var and independent_vars are empty
+        if not dependent_var or not independent_vars:
+            self.script_box.setPlainText("") 
             return
 
         # Pastikan dependent_var adalah string dan bersihkan tanda kutip
