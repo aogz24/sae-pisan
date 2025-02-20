bd24fef75a5569b80e7fb7f5ff874a109e753de7
cindy <cindy.septia0919@gmail.com>
2025-02-21 06:41:58 +0700

    refactor: Remove unused NormalityTestController and SummaryDataController

--- controller/Eksploration/NormalityTestController.py
+++
@@ -1,6 +0,0 @@
-class NormalityTestController:
-    def __init__(self,NormalityTestModel):
-        self.NormalityTestModel = NormalityTestModel
-
-    def run_model(self, r_script):
-        self.NormalityTestModel.run_model(r_script)
--- controller/Eksploration/SummaryDataController.py
+++
@@ -1,6 +0,0 @@
-class SummaryDataController:
-    def __init__(self, SummaryData):
-        self.SummaryData = SummaryData
-    
-    def run_model(self, r_script):
-        self.SummaryData.run_model(r_script)
