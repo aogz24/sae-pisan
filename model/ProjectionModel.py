ae0951b40bbf5dc5c778db5ed6da1e3e495e6a39
cindy <cindy.septia0919@gmail.com>
2025-03-12 10:14:15 +0700

    refactor: Update run_model method in ProjectionModel, SaeEblup, SaeEblupPseudo, SaeEblupUnit, and SaeHB

--- model/ProjectionModel.py
+++ model/ProjectionModel.py
@@ -4,11 +4,11 @@ from service.modelling.running_model.Projection import run_model_projection
 class Projection(SaeModelling):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.result = ""
     
     def run_model(self, r_script):
         self.r_script = r_script
-        run_model_projection(self)
+        result, error, df = run_model_projection(self)
+        return result, error, df
     
     def get_model2(self):
         return self.model2
--- model/SaeEblup.py
+++ model/SaeEblup.py
@@ -4,11 +4,11 @@ from service.modelling.running_model.SaeEblupArea import run_model_eblup_area
 class SaeEblup(SaeModelling):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.result = ""
     
     def run_model(self, r_script):
         self.r_script = r_script
-        run_model_eblup_area(self)
+        result, error, df = run_model_eblup_area(self)
+        return result, error, df
     
     def get_model2(self):
         return self.model2
