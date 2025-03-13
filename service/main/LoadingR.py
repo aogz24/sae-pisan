a6959134a3ac53a7f73d6eda9a83065674fdf94e
Agus Riyanto <ragus8188@gmail.com>
2025-03-13 07:41:53 +0700

    refactor: Add docstrings to environment check and R loading functions for improved documentation

--- service/main/LoadingR.py
+++ service/main/LoadingR.py
@@ -2,6 +2,16 @@ import rpy2.robjects as ro
 from rpy2.robjects import pandas2ri
 
 def loadR(splash):
+    """Loads and installs necessary R packages for the application.
+    This function updates the splash screen message, activates the pandas2ri interface,
+    and runs an R script to install required R packages if they are not already installed.
+    The R packages include 'sae', 'arrow', 'sae.projection', 'emdi', 'xgboost', 'LiblineaR',
+    'kernlab', 'GGally', 'ggplot2', 'ggcorrplot', 'car', and 'polars'.
+    Args:
+        splash: An object that handles the splash screen updates.
+    Returns:
+        None"""
+    
     splash.update_message()
     pandas2ri.activate()
     r_script = """
