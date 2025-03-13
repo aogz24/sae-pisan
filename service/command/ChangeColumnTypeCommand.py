610da34600c97944c4192edcc88f74c0c76ebef5
Agus Riyanto <ragus8188@gmail.com>
2025-03-13 07:37:08 +0700

    refactor: Add docstrings to command classes for improved documentation

--- service/command/ChangeColumnTypeCommand.py
+++ service/command/ChangeColumnTypeCommand.py
@@ -2,6 +2,21 @@ from PyQt6.QtGui import QUndoCommand
 import polars as pl
 
 class ChangeColumnTypeCommand(QUndoCommand):
+    """
+    A command class to change the data type of a column in a model, supporting undo and redo operations.
+    Attributes:
+        model (QAbstractTableModel): The model containing the data.
+        column_index (int): The index of the column to change.
+        old_dtype (str): The original data type of the column.
+        new_dtype (str): The new data type of the column.
+        old_data (list): The original data of the column.
+        new_data (list): The new data of the column.
+        column_name (str): The name of the column to change.
+    Methods:
+        undo(): Reverts the column to its original data type and data.
+        redo(): Changes the column to the new data type and data.
+    """
+    
     def __init__(self, model, column_index, old_dtype, new_dtype, old_data, new_data):
         super().__init__()
         self.model = model
