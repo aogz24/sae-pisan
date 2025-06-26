61deb9b21d04e42febaf5a750904f2d4e77be64c
Agus Riyanto <ragus8188@gmail.com>
2025-06-26 15:16:55 +0700

    feat: add dataset generation scripts for eblup-area, eblup_unit, proj, pseudo, and saeHB

--- cek.py
+++ cek.py
@@ -1,36 +1,3 @@
-import sys
-from PyQt6.QtWidgets import QApplication, QLabel, QPushButton, QVBoxLayout, QWidget
-from PyQt6.QtGui import QPixmap, QClipboard 
+import os
 
-class ClipboardImageApp(QWidget):
-    def __init__(self):
-        super().__init__()
-
-        # Load gambar ke QPixmap
-        self.pixmap = QPixmap("assets/icon.svg")
-
-        # Label untuk menampilkan gambar
-        self.label = QLabel()
-        self.label.setPixmap(self.pixmap)
-
-        # Tombol untuk menyalin ke clipboard
-        self.copy_button = QPushButton("Copy to Clipboard")
-        self.copy_button.clicked.connect(self.copy_to_clipboard)
-
-        # Layout
-        layout = QVBoxLayout()
-        layout.addWidget(self.label)
-        layout.addWidget(self.copy_button)
-        self.setLayout(layout)
-
-    def copy_to_clipboard(self):
-        clipboard = QApplication.clipboard()
-        clipboard.setPixmap(self.pixmap, QClipboard.Mode.Clipboard)  # Menyalin gambar ke clipboard
-
-        print("Image copied to clipboard!")
-
-# Jalankan aplikasi
-app = QApplication(sys.argv)
-window = ClipboardImageApp()
-window.show()
-sys.exit(app.exec())
+print(os.getenv("APPDATA"))
