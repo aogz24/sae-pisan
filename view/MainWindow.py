5159001aff383568a97244029f3ce22d3762b2d6
Agus Riyanto <ragus8188@gmail.com>
2025-07-19 12:20:30 +0700

    fix: update application version in window title to v1.5.0

--- view/MainWindow.py
+++ view/MainWindow.py
@@ -131,7 +131,7 @@ class MainWindow(QMainWindow):
         
         super().__init__()
 
-        self.setWindowTitle("saePisan: Small Area Estimation Programming for Statistical Analysis v1.4.0")
+        self.setWindowTitle("saePisan: Small Area Estimation Programming for Statistical Analysis v1.5.0")
         columns = [f"Column {i+1}" for i in range(100)]
         self.data1 = pl.DataFrame({col: [None] * 100 for col in columns})
         self.data2 = pl.DataFrame({
